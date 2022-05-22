a0864907d9c778b218ab246520f6bd3fb46626ff
崔 明辉 <cuis@vip.qq.com>
2022-05-22 19:33:54 +0800

    refactor: Wechat share logic.

--- packages/mpflutter_sample/lib/layouts/container_page.dart
+++ packages/mpflutter_sample/lib/layouts/container_page.dart
@@ -43,8 +43,141 @@ class ContainerPage extends StatelessWidget {
         title: Text('Hello'),
       ),
       backgroundColor: Color.fromARGB(255, 236, 236, 236),
-      body: Center(
-        child: MyStatefulWidget(),
+      // onWechatMiniProgramShareAppMessage: (request) async {
+      //   return MPWechatMiniProgramShareInfo(
+      //     title: 'Container 容器标题(${request.from ?? ''})',
+      //     imageUrl:
+      //         'https://www-jsdelivr-com.onrender.com/img/landing/built-for-production-icon@2x.png',
+      //   );
+      // },
+      body: ListView(
+        children: [
+          _renderBlock(Column(
+            children: [
+              _renderHeader('Container with color and size.'),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.pink,
+              ),
+              SizedBox(height: 16),
+            ],
+          )),
+          _renderBlock(Column(
+            children: [
+              _renderHeader('Container with Center Container'),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.pink,
+                child: Center(
+                  child: Container(
+                    width: 44,
+                    height: 44,
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+            ],
+          )),
+          _renderBlock(Column(
+            children: [
+              _renderHeader('Container with alignment (topRight)'),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.pink,
+                alignment: Alignment.topRight,
+                child: Container(
+                  width: 44,
+                  height: 44,
+                  color: Colors.yellow,
+                ),
+              ),
+              SizedBox(height: 16),
+            ],
+          )),
+          _renderBlock(Column(
+            children: [
+              _renderHeader('Container with padding'),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.pink,
+                padding: EdgeInsets.all(12),
+                child: Container(
+                  width: 44,
+                  height: 44,
+                  color: Colors.yellow,
+                ),
+              ),
+              SizedBox(height: 16),
+            ],
+          )),
+          _renderBlock(Column(
+            children: [
+              _renderHeader('Container with decoration'),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  border: Border.all(width: 4, color: Colors.black),
+                  borderRadius: BorderRadius.circular(22),
+                ),
+              ),
+              SizedBox(height: 16),
+            ],
+          )),
+          _renderBlock(Column(
+            children: [
+              _renderHeader('Container with foregroundDecoration'),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  border: Border.all(width: 4, color: Colors.black),
+                  borderRadius: BorderRadius.circular(22),
+                ),
+                foregroundDecoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.transparent,
+                      Colors.black,
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(22),
+                ),
+              ),
+              SizedBox(height: 16),
+            ],
+          )),
+          _renderBlock(Column(
+            children: [
+              _renderHeader('Container with border and center text'),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  border: Border.all(width: 4, color: Colors.black),
+                  borderRadius: BorderRadius.circular(22),
+                ),
+                child: Center(
+                  child: Text(
+                    'Hello',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+            ],
+          )),
+        ],
       ),
     );
   }
